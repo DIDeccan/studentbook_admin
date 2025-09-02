8795ff8a1535aa60636c5066ca92f8f69d62f8e7
pramodkumar0813 <pramodkumarbandi2@gmail.com>
2025-09-02 14:38:09 +0530

    piechart

--- Backend/dist/studentbookadminbackend/settings.py
+++ Backend/dist/studentbookadminbackend/settings.py
@@ -75,11 +75,19 @@ WSGI_APPLICATION = 'studentbookadminbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',   # or mysql, sqlite3, oracle
+        'NAME': 'studentbook_db',
+        'USER': 'postgres',
+        'PASSWORD': 'student',
+        'HOST': '192.168.0.6',   # or IP, e.g. '127.0.0.1'
+        'PORT': '5432',          # default for PostgreSQL
     }
 }
 
+
+MIGRATION_MODULES = {
+    'studentbookadminfrontend': None,   # disables migrations for this app
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- Backend/dist/studentbookadminfrontend/models.py
+++ Backend/dist/studentbookadminfrontend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Class(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=2000)
+    description = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.name
+    class Meta:
+        db_table = "studentbookfrontend_class"
+ 
+ 
